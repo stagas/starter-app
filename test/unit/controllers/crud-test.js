370d95b30082171e8d3132acd182314e08c04a41
George Stagas <gstagas@gmail.com>
2017-05-07 20:00:21 +0200

    tests: fix crud test

--- test/unit/controllers/crud-test.js
+++ test/unit/controllers/crud-test.js
@@ -18,31 +18,31 @@ test('crud', t => {
 })
 
 test('crud.list', async t => {
-  let ctx = { services, modelName: 'posts' }
+  let ctx = { services, params: { model: 'Post' } }
   await crud.list(ctx)
   t.truthy(services.crud.list.calledOnce)
 })
 
 test('crud.show', async t => {
-  let ctx = { services, modelName: 'posts', id: 1 }
+  let ctx = { services, params: { model: 'Post', id: 1 } }
   await crud.show(ctx)
   t.truthy(services.crud.show.calledOnce)
 })
 
 test('crud.create', async t => {
-  let ctx = { services, modelName: 'posts', req: { body: { foo: 'bar' }} }
+  let ctx = { services, params: { model: 'Post' }, request: { body: { foo: 'bar' }} }
   await crud.create(ctx)
   t.truthy(services.crud.create.calledOnce)
 })
 
 test('crud.update', async t => {
-  let ctx = { services, modelName: 'posts', id: 1, req: { body: { foo: 'baz' }}}
+  let ctx = { services, params: { model: 'Post', id: 1 }, request: { body: { foo: 'baz' }}}
   await crud.update(ctx)
   t.truthy(services.crud.update.calledOnce)
 })
 
 test('crud.delete', async t => {
-  let ctx = { services, modelName: 'posts', id: 1, throw: () => {} }
+  let ctx = { services, params: { model: 'Post', id: 1 }, throw: () => {} }
   await crud.delete(ctx)
   t.truthy(services.crud.delete.calledOnce)
 })
