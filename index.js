0a7167c0e78837ad975ab9836e0b0bfec7a9a525
George Stagas <gstagas@gmail.com>
2017-05-08 21:30:38 +0200

    feat: pass swagger json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import { loadDocumentSync } from 'swagger2'
 import starter from '@blended/starter'
 import config from './config'
 import api from './api'
 
-let init = () => starter(api, config)
+let swagger = loadDocumentSync(config.swagger)
+
+let init = () => starter(swagger, api, config)
 
 export default init
 
